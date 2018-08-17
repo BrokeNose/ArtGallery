4fea5d1d4d9f3fea69dc65d57d3fe4f33fa89c23
Daewon_Son <Daewon_Son@DESKTOP-SMLJG5H>
2018-08-17 17:44:29 +0900

    project basic setting

---
+++ ArtGallery/src/main/java/com/main/artgallery/HomeController.java
@@ -0,0 +1,12 @@
+package com.main.artgallery;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/home.do")
+	public String home() {
+		return "home";
+	}
+}
