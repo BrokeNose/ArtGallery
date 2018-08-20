626a46d02d052d0575422dfe7c6a83113f853365
acorn <acorn@acorn-PC>
2018-08-20 18:03:27 +0900

    shong 8월 20일 Artist.xml update-1

--- ArtGallery/src/main/java/com/main/artgallery/HomeController.java
+++ ArtGallery/src/main/java/com/main/artgallery/HomeController.java
@@ -1,12 +1,17 @@
 package com.main.artgallery;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/home.do")
-	public String home() {
+//	public String home(HttpServletRequest request) {
+//		String rPath = request.getSession().getServletContext().getRealPath("/upload");
+//		System.out.println(rPath);
+	public String home() {	
 		return "home";
 	}
 }
