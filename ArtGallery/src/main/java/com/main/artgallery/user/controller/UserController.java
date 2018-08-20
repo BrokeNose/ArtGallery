f99d9ec3d0769da4d47cb4486c22d2465166f277
autumnalkitty <nora.matamune@gmail.com>
2018-08-20 18:31:51 +0900

    updating user feature

--- ArtGallery/src/main/java/com/main/artgallery/user/controller/UserController.java
+++ ArtGallery/src/main/java/com/main/artgallery/user/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.main.artgallery.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.main.artgallery.user.service.UserService;
 
 @Controller
 public class UserController {
+	@Autowired
+	private UserService uSerivce;
 	
 }
---
+++ ArtGallery/src/main/java/com/main/artgallery/user/service/UserServiceImpl.java
@@ -0,0 +1,82 @@
+package com.main.artgallery.user.service;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.main.artgallery.user.dao.UserDao;
+import com.main.artgallery.user.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserDao dao;
+	@Override
+	public boolean canUseId(String id) {
+		return dao.canUseId(id);
+	}
+	@Override
+	public void signUp(ModelAndView mView, UserDto dto) {
+		BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+		String encodedPwd=encoder.encode(dto.getPwd());
+		dto.setPwd(encodedPwd);
+		dao.insert(dto);
+		mView.addObject("msg", dto.getId()+" 회원님 가입됐습니다.");
+	}
+	@Override
+	public void signIn(ModelAndView mView, UserDto dto, HttpSession session) {
+		UserDto resultDto=dao.getData(dto.getId());
+		boolean isSigninSuccess=false;
+		if(resultDto!=null) {
+			isSigninSuccess=BCrypt.checkpw(dto.getPwd(), resultDto.getPwd());
+		}
+		if(isSigninSuccess) {
+			session.setAttribute("id", dto.getId());
+		}
+		mView.addObject("isSigninSuccess", isSigninSuccess);
+	}
+	@Override
+	public void info(ModelAndView mView, HttpSession session) {
+		String id=(String)session.getAttribute("id");
+		UserDto dto=dao.getData(id);
+		mView.addObject("dto", dto);
+	}
+	@Override
+	public void updateForm(ModelAndView mView, HttpSession session) {
+		String id=(String)session.getAttribute("id");
+		UserDto dto=dao.getData(id);
+		mView.addObject("dto", dto);
+	}
+	@Override
+	public void update(UserDto dto) {
+		dao.update(dto);
+	}
+	@Override
+	public boolean isValidPwd(String inputPwd, HttpSession session) {
+		String id=(String)session.getAttribute("id");
+		UserDto dto=dao.getData(id);
+		boolean isValid=BCrypt.checkpw(inputPwd, dto.getPwd());
+		return isValid;
+	}
+	@Override
+	public void changePwd(String pwd, HttpSession session) {
+		String id=(String)session.getAttribute("id");
+		BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+		String encodedPwd=encoder.encode(pwd);
+		UserDto dto=new UserDto();
+		dto.setId(id);
+		dto.setPwd(encodedPwd);
+		dao.changePwd(dto);
+	}
+	@Override
+	public void delete(ModelAndView mView, HttpSession session) {
+		String id=(String)session.getAttribute("id");
+		dao.delete(id);
+		session.invalidate();
+		mView.addObject("msg", id+" 회원님 탈퇴됐습니다.");
+	}
+}
