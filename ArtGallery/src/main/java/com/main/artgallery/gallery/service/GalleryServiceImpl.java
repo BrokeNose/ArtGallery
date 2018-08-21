87a15cfaff76dd037bd2c440ec6be016f5535e14
Daewon_Son <Daewon_Son@DESKTOP-SMLJG5H>
2018-08-21 17:30:11 +0900

    Son- GalleryServiceImpl 주석 제거 입니다. 테스트용

--- ArtGallery/src/main/java/com/main/artgallery/gallery/service/GalleryServiceImpl.java
+++ ArtGallery/src/main/java/com/main/artgallery/gallery/service/GalleryServiceImpl.java
@@ -17,22 +17,15 @@ public class GalleryServiceImpl implements GalleryService {
 	public void getList(HttpServletRequest request,ModelAndView mView) {
 		String categoryType=(String)request.getParameter("categorytype");
 		
-		System.out.println("11111111111111");
 		if (categoryType==null) {
 			
 		}else if(categoryType.equals("A")) {
-			System.out.println("222222222222222");
 			mView.addObject("list",dao.AGetList());
-			System.out.println("555555555555555");
 			
 		}else if(categoryType.equals("M")) {
-			System.out.println("333333333333333333");
 			mView.addObject("list",dao.MGetList());
-			System.out.println("666666666666666");
 		}else if(categoryType.equals("P")) {
-			System.out.println("4444444444444");
 			mView.addObject("list",dao.PGetList());
-			System.out.println("6666666666666666");
 		}
 	}
 }
