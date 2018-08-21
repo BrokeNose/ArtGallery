3fa9a4da65db67f11d06142893428148eaca2fef
acorn <acorn@acorn-PC>
2018-08-21 20:06:22 +0900

    test commit

--- ArtGallery/src/main/java/com/main/artgallery/gallery/service/GalleryService.java
+++
@@ -1,9 +0,0 @@
-package com.main.artgallery.gallery.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.web.servlet.ModelAndView;
-
-public interface GalleryService {
-	public void getList(HttpServletRequest request,ModelAndView mView);
-}
