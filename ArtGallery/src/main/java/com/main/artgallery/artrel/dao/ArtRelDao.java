e193b7f62745f7e36d81d6565a34320e7b101e0b
doodr <doodr@DESKTOP-8TTVH1O>
2018-08-22 16:13:00 +0900

    /views/admin/   folder  created ArtDto, Mapper, Controller, Service  modified HomeController, Home.jsp  modified

--- ArtGallery/src/main/java/com/main/artgallery/artrel/dao/ArtRelDao.java
+++ ArtGallery/src/main/java/com/main/artgallery/artrel/dao/ArtRelDao.java
@@ -6,7 +6,7 @@ import com.main.artgallery.artrel.dto.ArtRelDto;
 
 /*
  * 작성자 : hyung
- * 비고 : 자료 관리는 연계자료 delete 후 insert하는 방식으로 처리 됨. 그래서  update()가 없음
+ * 비고 : 자료 관리는 연계자료 delete 후 insert하는 방식으로 처리 됨. 그래서  update가 없음
  */
 
 public interface ArtRelDao {
