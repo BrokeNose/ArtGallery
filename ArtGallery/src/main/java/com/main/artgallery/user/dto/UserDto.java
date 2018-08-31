ea30550e54f2b3dcac09e610964403f770f2d051
acorn <acorn@acorn-PC>
2018-08-31 18:36:54 +0900

    shong update

--- ArtGallery/src/main/java/com/main/artgallery/user/dto/UserDto.java
+++ ArtGallery/src/main/java/com/main/artgallery/user/dto/UserDto.java
@@ -10,11 +10,14 @@ public class UserDto {
 	private String rollName;
 	private int startRowNum;
 	private int endRowNum;
+	private int userCount;
+	private int adminCount;
+	private int allCount;
 	
 	public UserDto() {}
 
 	public UserDto(String id, String pwd, String email, String roll, String regdate, String deldate, String rollName,
-			int startRowNum, int endRowNum) {
+			int startRowNum, int endRowNum, int userCount, int adminCount, int allCount) {
 		super();
 		this.id = id;
 		this.pwd = pwd;
@@ -25,59 +28,104 @@ public class UserDto {
 		this.rollName = rollName;
 		this.startRowNum = startRowNum;
 		this.endRowNum = endRowNum;
+		this.userCount = userCount;
+		this.adminCount = adminCount;
+		this.allCount = allCount;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPwd() {
 		return pwd;
 	}
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getRoll() {
 		return roll;
 	}
+
 	public void setRoll(String roll) {
 		this.roll = roll;
 	}
+
 	public String getRegdate() {
 		return regdate;
 	}
+
 	public void setRegdate(String regdate) {
 		this.regdate = regdate;
 	}
+
 	public String getDeldate() {
 		return deldate;
 	}
+
 	public void setDeldate(String deldate) {
 		this.deldate = deldate;
 	}
+
 	public String getRollName() {
 		return rollName;
 	}
+
 	public void setRollName(String rollName) {
 		this.rollName = rollName;
 	}
+
 	public int getStartRowNum() {
 		return startRowNum;
 	}
+
 	public void setStartRowNum(int startRowNum) {
 		this.startRowNum = startRowNum;
 	}
+
 	public int getEndRowNum() {
 		return endRowNum;
 	}
+
 	public void setEndRowNum(int endRowNum) {
 		this.endRowNum = endRowNum;
-	}		
+	}
+
+	public int getUserCount() {
+		return userCount;
+	}
+
+	public void setUserCount(int userCount) {
+		this.userCount = userCount;
+	}
+
+	public int getAdminCount() {
+		return adminCount;
+	}
+
+	public void setAdminCount(int adminCount) {
+		this.adminCount = adminCount;
+	}
+
+	public int getAllCount() {
+		return allCount;
+	}
+
+	public void setAllCount(int allCount) {
+		this.allCount = allCount;
+	}
 }
